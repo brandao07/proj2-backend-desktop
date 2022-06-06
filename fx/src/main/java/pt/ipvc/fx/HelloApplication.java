815f6c6932bc3d43e499adde32776eb5fb8dfd08
Hugo Costa <hugocosta9898@gmail.com>
2022-06-06 20:41:03 +0100

    admin adicionar dados

--- fx/src/main/java/pt/ipvc/fx/HelloApplication.java
+++ fx/src/main/java/pt/ipvc/fx/HelloApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import pt.ipvc.backend.bll.UtilizadorBLL;
 import pt.ipvc.fx.controller.ControladorGlobal;
 
 import java.io.IOException;
