47f294d79f63861ce0e500ee24c31094d9467c7b
Hugo Costa <hugocosta9898@gmail.com>
2022-06-11 17:36:04 +0100

    icones

---
+++ fx/src/main/java/pt/ipvc/fx/controller/Administrador/consultar_editarDados/ConsultarEditarDadosModalidadeController.java
@@ -0,0 +1,35 @@
+package pt.ipvc.fx.controller.Administrador.consultar_editarDados;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class ConsultarEditarDadosModalidadeController implements Initializable {
+    @FXML
+    private ChoiceBox itemPesquisar;
+
+    @FXML
+    private Button btnPesquisar;
+
+    @FXML
+    private TextField textFieldpesquisa;
+
+    @FXML
+    private TableView tableModalidades;
+
+    @FXML
+    private Button btnDetalhes;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        itemPesquisar.getItems().addAll("Árbitros", "Atletas", "Equipa",  "Modalidade", "Prêmio", "Recintos", "Tipos de Recinto", "Tipos de Prêmio");
+
+    }
+}
