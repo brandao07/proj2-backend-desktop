6d9cea4d545882e7634e25b7bd9804658bb67179
hugocosta <hugocosta9898@gmail.com>
2022-06-21 18:52:50 +0100

    commit top

--- fx/src/main/java/pt/ipvc/fx/controller/Administrador/AdminHomePageController.java
+++ fx/src/main/java/pt/ipvc/fx/controller/Administrador/AdminHomePageController.java
@@ -36,5 +36,6 @@ public class AdminHomePageController {
 
     public void setBtnAdicionarDados(ActionEvent event) {
         ControladorGlobal.chamaScene("adicionarDados/admin-adicionar-dados-arbitro.fxml", event);
+
     }
 }
